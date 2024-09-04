a8edeb7e8f7f29d2642ebb4d3f644faf3036f73e
Victoria Cano <victoriacano@outlook.es>
2024-09-03 23:13:55 -0500

    ejercicio finalizado

--- Ejercicio1_Coordenadas/Program.cs
+++ Ejercicio1_Coordenadas/Program.cs
@@ -12,16 +12,16 @@
         //1 forma de inicializar
 
         //inicializar variables pidiendolas por pantalla 
-        Console.WriteLine("Ingrese la primer coordenaada");
+        Console.WriteLine("Ingrese la primer coordenaada x1: ");
         x1 = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("Ingrese la primer coordenaada");
+        Console.WriteLine("Ingrese la segunda coordenaada y1: ");
         y1 = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("Ingrese la primer coordenaada");
+        Console.WriteLine("Ingrese la tercera coordenaada x2: ");
         x2 = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("Ingrese la primer coordenaada");
+        Console.WriteLine("Ingrese la cuarta coordenaada y2: ");
         y2 = Convert.ToDouble(Console.ReadLine());
 
         double x = (x2 - x1) * (x2 - x1);
@@ -30,7 +30,7 @@
         
         d = Math.Sqrt(suma);
 
-        Console.WriteLine("d");
+        Console.Write(suma);
 
 
 
