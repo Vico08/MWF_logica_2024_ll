e6d779b3f8b0cb18b63ff1279568bd572338788a
Victoria Cano <victoriacano@outlook.es>
2024-09-03 23:04:51 -0500

    guardamos avances

--- Ejercicio1_Coordenadas/Program.cs
+++ Ejercicio1_Coordenadas/Program.cs
@@ -15,5 +15,24 @@
         Console.WriteLine("Ingrese la primer coordenaada");
         x1 = Convert.ToDouble(Console.ReadLine());
 
+        Console.WriteLine("Ingrese la primer coordenaada");
+        y1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la primer coordenaada");
+        x2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la primer coordenaada");
+        y2 = Convert.ToDouble(Console.ReadLine());
+
+        double x = (x2 - x1) * (x2 - x1);
+        double y = (y2 - y1) * (y2 - y1);
+        double suma = x + y;
+        
+        d = Math.Sqrt(suma);
+
+        Console.WriteLine("d");
+
+
+
     }
 }
