254326a09b7bf3564518aa6f77106a05efe4d38d
Victoria Cano <victoriacano@outlook.es>
2024-09-03 22:39:42 -0500

    guardakos avances

--- Ejercicio1_Coordenadas/Program.cs
+++ Ejercicio1_Coordenadas/Program.cs
@@ -2,6 +2,18 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+
+        double x1, x2, y1, y2, d;
+        //x1 = 4;
+        //x2 = 5;
+        //y1 = 5.2;
+        //y2 = 15.58;
+        //1 forma de inicializar
+
+        //inicializar variables pidiendolas por pantalla 
+        Console.WriteLine("Ingrese la primer coordenaada");
+        x1 = Convert.ToDouble(Console.ReadLine());
+
     }
 }
